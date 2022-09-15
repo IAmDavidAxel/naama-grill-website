31125b22ce8a9915f7b4595f2bd878366345c760
IAmDavidAxel <david-axel-wendbenedo.1@ulaval.ca>
2022-09-15 12:03:04 -0400

    refactored  website added firebase resources. WIP: CI/CD with github action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgsRevealConfig} from "ngx-scrollreveal";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'naama-grill-website';
+
+  constructor(config: NgsRevealConfig) {
+    // customize default values of ngx-scrollreveal directives used by this component tree
+    config.duration = 5000;
+    config.easing = 'cubic-bezier(0.645, 0.045, 0.355, 1)';
+
+    config.reset = true;
+
+    //other options here
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterModule} from "@angular/router";
+import {NgsRevealModule} from "ngx-scrollreveal";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    NgsRevealModule,
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
